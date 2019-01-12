903302a4bbaa15914fb22a7c89a2786cfbe3034f
shinjaekwang <shinjawkwang@naver.com>
2019-01-12 23:52:49 +0900

    modulization wsgi

---
+++ ddrakProject/wsgi/debug.py
@@ -0,0 +1,20 @@
+"""
+WSGI config for lala project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ddrakProject.settings.debug")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()

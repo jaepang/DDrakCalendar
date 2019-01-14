04c4e74c9613f383cc30fb038126e341652d5c25
Ubuntu <ubuntu@ip-172-31-46-123.us-east-2.compute.internal>
2019-01-14 18:10:56 +0000

    whitenoise disabled; https disabled

--- ddrakProject/settings/base.py
+++ ddrakProject/settings/base.py
@@ -1,10 +1,10 @@
 import os
 
 # apply https
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
+SECURE_SSL_REDIRECT = False
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
 
 PROJECT_PATH = os.path.realpath((os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 

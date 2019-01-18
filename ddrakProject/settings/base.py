9ac4c22680503ee4d294bb10dfe597aa6da27451
shinjaekwang <shinjawkwang@naver.com>
2019-01-19 03:31:44 +0900

    https disabled

--- ddrakProject/settings/base.py
+++ ddrakProject/settings/base.py
@@ -2,10 +2,10 @@ import os
 import json
 
 # apply https
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
+# SECURE_SSL_REDIRECT = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
 
 PROJECT_PATH = os.path.realpath((os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 

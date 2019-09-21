ef7820d07e97d99fc5677c1681704572c3fc0947
jkpraha <shinjawkwang@naver.com>
2019-09-21 23:30:33 +0900

    DEBUG turned off

--- ddrakProject/settings/deploy.py
+++ ddrakProject/settings/deploy.py
@@ -2,7 +2,7 @@ from .base import *
 import dj_database_url
 from whitenoise.django import DjangoWhiteNoise
 
-DEBUG = True
+DEBUG = False
 WSGI_APPLICATION = 'ddrakProject.wsgi.deploy.application'
 
 WSGI_APPLICATION = DjangoWhiteNoise(WSGI_APPLICATION)

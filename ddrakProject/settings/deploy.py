939ad94a6896c7475d8723c275f5e4a2e539a42c
jkpraha <shinjawkwang@naver.com>
2019-09-21 14:20:29 +0900

    Remove duplicated import

--- ddrakProject/settings/deploy.py
+++ ddrakProject/settings/deploy.py
@@ -9,6 +9,5 @@ WSGI_APPLICATION = DjangoWhiteNoise(WSGI_APPLICATION)
 
 
 # Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)

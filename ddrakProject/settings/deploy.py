17a283a266f4a783066fa224e35a39ff1c67b818
jkpraha <shinjawkwang@naver.com>
2019-09-21 14:17:19 +0900

    Deployment db setting added

--- ddrakProject/settings/deploy.py
+++ ddrakProject/settings/deploy.py
@@ -6,3 +6,9 @@ DEBUG = False
 WSGI_APPLICATION = 'ddrakProject.wsgi.deploy.application'
 
 WSGI_APPLICATION = DjangoWhiteNoise(WSGI_APPLICATION)
+
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)

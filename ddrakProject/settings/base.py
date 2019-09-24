dd2fbd4addc3d2c20995132e94aeeec5fa140d23
jkpraha <shinjawkwang@gmail.com>
2019-09-24 22:32:36 +0900

    https activate

--- ddrakProject/settings/base.py
+++ ddrakProject/settings/base.py
@@ -1,13 +1,12 @@
 import os
 import json
 
-# apply https -> temporarilly disable before we get ssl certification
-'''
+# apply https
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
-'''
+
 
 PROJECT_PATH = os.path.realpath((os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 

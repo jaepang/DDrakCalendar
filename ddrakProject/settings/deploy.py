2a142388e05c3de1cbd114e4936bdecfb9a6e9f7
root <shinjawkwang@naver.com>
2019-09-25 14:37:09 +0000

    secretkey configuration

--- ddrakProject/settings/deploy.py
+++ ddrakProject/settings/deploy.py
@@ -2,6 +2,9 @@ from .base import *
 import dj_database_url
 from whitenoise.django import DjangoWhiteNoise
 
+# SECRET KEY is saved in Heroku config variable.
+SECRET_KEY = os.environ['SECRET_KEY']
+
 DEBUG = False
 WSGI_APPLICATION = 'ddrakProject.wsgi.deploy.application'
 WSGI_APPLICATION = DjangoWhiteNoise(WSGI_APPLICATION)
@@ -10,6 +13,3 @@ WSGI_APPLICATION = DjangoWhiteNoise(WSGI_APPLICATION)
 # Heroku: Update database configuration from $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-
-# SECRET KEY is saved in Heroku config variable.
-SECRET_KEY = os.environ['SECRET_KEY']

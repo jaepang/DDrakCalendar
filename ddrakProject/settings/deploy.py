2304887c5c7b743b9f1dc2a27880c0867959f695
root <shinjawkwang@naver.com>
2019-09-26 14:56:37 +0000

    settings files comment improved

--- ddrakProject/settings/deploy.py
+++ ddrakProject/settings/deploy.py
@@ -6,8 +6,9 @@ from whitenoise.django import DjangoWhiteNoise
 Used when deploying by heroku.
 to deploy (on heroku): push github and just press "deploy branch" button.
 difference from debug: 1. DEBUG is False
-                       2. STATIC_ROOT instead of STATICFILES_DIRS
+                       2. STATIC_ROOT instead of STATICFILES_DIRS.
                        ** STATIC_ROOT and STATICFILES_DIRS cannot be dupplicated!!
+                       3. database setting for heroku exists.
 '''
 
 DEBUG = False

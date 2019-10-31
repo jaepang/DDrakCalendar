bff28fb7558c0e4404a92549cbb10fb45b5ba482
root <shinjawkwang@naver.com>
2019-10-31 10:16:39 +0000

    debug rolled back

--- ddrakProject/settings/debug.py
+++ ddrakProject/settings/debug.py
@@ -9,7 +9,8 @@ difference from deploy: 1. DEBUG is True
                         3. database setting for heroku doesn't exist.
 '''
 
-DEBUG = False # temporarilly changed for 404 implementation
+DEBUG = True
+
 STATICFILES_DIRS = (
     # There should be ','!!
     os.path.join(PROJECT_PATH, 'assets'),
